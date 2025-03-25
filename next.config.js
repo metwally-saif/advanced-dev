ee8cc41e23da4696413371cb657cb88abb2aacdc
Saif Ayman Fathi Mohamed Metwally <65094581+metwally-saif@users.noreply.github.com>
2025-03-25 11:07:36 +0200

    performance commit

--- next.config.js
+++ next.config.js
@@ -3,11 +3,38 @@
  */
 module.exports = {
   experimental: {
+    ppr: true,
+    inlineCss: true,
+    reactCompiler: true,
     serverActions: {
       allowedOrigins: ["app.localhost:3000"],
     },
   },
   images: {
+    minimumCacheTTL: 31536000,
     remotePatterns: [{ hostname: "*" }],
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/insights/vitals.js",
+        destination:
+          "https://cdn.vercel-insights.com/v1/speed-insights/script.js",
+      },
+      {
+        source: "/insights/events.js",
+        destination: "https://cdn.vercel-insights.com/v1/script.js",
+      },
+      {
+        source: "/hfi/events/:slug*",
+        destination:
+          "https://vitals.vercel-insights.com/v1/:slug*?dsn=KD0ni5HQVdxsHAF2tqBECObqH",
+      },
+      {
+        source: "/hfi/vitals",
+        destination:
+          "https://vitals.vercel-insights.com/v2/vitals?dsn=fsGnK5U2NRPzYx0Gch0g5w5PxT1",
+      },
+    ];
   },
 };
